6b937a2e402807bdaed67d244a9aba4af9083305
Sneha <gond0017@algonquinlive.com>
2020-10-07 19:27:30 -0400

    Updates

---
+++ precache-manifest.ebd764c7983a01ea2c876aa6cd3764f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12145e7d79b5daac00785b0748e2055",
+    "url": "/mad9135-p1-react-crud-list/index.html"
+  },
+  {
+    "revision": "3ea5e0228a8fccd698f4",
+    "url": "/mad9135-p1-react-crud-list/static/css/main.dfb66c3e.chunk.css"
+  },
+  {
+    "revision": "7a422665f92ba79e0e59",
+    "url": "/mad9135-p1-react-crud-list/static/js/2.8fd870b7.chunk.js"
+  },
+  {
+    "revision": "f9768baf02b23900e81c8723e7201596",
+    "url": "/mad9135-p1-react-crud-list/static/js/2.8fd870b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ea5e0228a8fccd698f4",
+    "url": "/mad9135-p1-react-crud-list/static/js/main.8f438593.chunk.js"
+  },
+  {
+    "revision": "7363d5974df73acaa7f0",
+    "url": "/mad9135-p1-react-crud-list/static/js/runtime-main.7e207898.js"
+  }
+]);
